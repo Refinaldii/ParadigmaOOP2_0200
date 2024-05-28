0d3c817b82756d64c6afcbb50b90e69d037033e3
Refinaldii <refinaldi9786@gmail.com>
2024-05-28 10:20:14 +0700

    membuat class joko

--- ParadigmaOOP2_0200.cpp
+++ ParadigmaOOP2_0200.cpp
@@ -4,12 +4,25 @@ using namespace std;
 class seseorang
 {
 public:
+    // pure virtual function
     virtual void pesan() = 0;
+
     /*virtual void pesan()
      {
      cout << "pesan dari seseorang" << endl;
      }*/
 
+};
+
+class joko : public seseorang
+{
+public:
+    // deklarasi
+    void pesan()
+    {
+        //implementasi
+        cout << "Pesan dari joko " << endl;
+    }
 };
 
 int main()
