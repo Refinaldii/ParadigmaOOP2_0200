beb4583f925bc58a718389362f6629fae3ab921d
Refinaldii <refinaldi9786@gmail.com>
2024-05-28 11:20:54 +0700

    membuat class orang dalam lingkup public

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 class orang {
 public:
+    int umur;
 
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
 };
 int main()
 {
