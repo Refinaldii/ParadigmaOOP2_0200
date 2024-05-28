1955860e7cd535e13137011a6ff0c26558f76cbb
Refinaldii <refinaldi9786@gmail.com>
2024-05-28 10:44:16 +0700

    membuat class baru derived class dan membuat nya jdi lingkup public

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -10,6 +10,15 @@ public:
     }
 };
 
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
