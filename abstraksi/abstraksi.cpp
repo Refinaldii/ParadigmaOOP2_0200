e506a088851471c0d8a49658c67328b4828f32a1
Refinaldii <refinaldi9786@gmail.com>
2024-05-28 11:07:22 +0700

    membuat class abstraksis dan juga membuat 2 lingkup private dan public

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Abstraksiklas {
+private: string x, y;
 
+public:
+    
+    //method untuk mengisi nilai
+    //priavte member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
