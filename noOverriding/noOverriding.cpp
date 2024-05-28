05f7ad76568d7269120e38725ae65b7a56fbeb18
Refinaldii <refinaldi9786@gmail.com>
2024-05-28 10:32:01 +0700

    membuat lingkup public

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 
+// class parent 
+// tambahkan final sesudah nama class
+// untuk mencegah adanya overriding
+
+class baseClass
+{
+public:
+
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
