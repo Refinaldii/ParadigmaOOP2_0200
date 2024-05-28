e4555648a101e4878711e256f10ee6989a8e1212
Refinaldii <refinaldi9786@gmail.com>
2024-05-28 10:24:22 +0700

    membuat class lia

--- ParadigmaOOP2_0200.cpp
+++ ParadigmaOOP2_0200.cpp
@@ -25,6 +25,15 @@ public:
     }
 };
 
+class lia : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari lia " << endl;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
